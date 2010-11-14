d876f3c7113ac6cc21a386da6fb0a453d1d49835
maze <maze@pyth0n.org>
2010-11-14 18:06:56 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'getent',
+    version      = '0.1',
+    author       = 'Wijnand Modderman-Lenstra',
+    author_email = 'maze@pyth0n.org',
+    description  = 'Python interface to the POSIX getent family of commands',
+    long_description = file('README.rst').read(),
+    license      = 'MIT',
+    keywords     = 'getent group passwd shadow network alias host',
+    packages     = ['getent'],
+)
+
